3baaddf63d3ccd6970f8f89a7da115c937b557c9
Jon Schulman <jon@js-tbpro13.local>
2019-06-24 15:29:48 -0700

    fix broken curly brace

--- main.tf
+++ main.tf
@@ -21,3 +21,4 @@ data "aws_ami" "ubuntu" {
     resource "aws_instance" "web" {
         ami                 =       "${data.aws_ami.ubuntu.id}"
         instance_type       =       "t2.micro"
+    }
